aba4554688544ee5e2806b1a0ffc7a98a0721af8
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-25 20:33:10 +0100

    Exports couple.

---
+++ src/export.js
@@ -0,0 +1,9 @@
+require('coffee-script/register');
+module.exports = {
+    couple: require('./couple.coffee'),
+    field: require('./field.coffee'),
+    match: require('./match.coffee'),
+    merge: require('./merge.coffee'),
+    type: require('./type.coffee'),
+    validate: require('./validate.coffee')
+};
